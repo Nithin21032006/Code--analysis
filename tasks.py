9317580ab5d4b0c0bcaf6de3bcdb027f41acff7c
Nithin.D <nithu210306@gmail.com>
2026-04-08 21:09:00 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,12 +2,17 @@ TASKS = {
     "easy": {
         "id": "easy_001",
         "difficulty": "easy",
+        "grader": True,
+        "reward_range": (0.01, 0.99),
         "code": "print('Hello World'",
         "issue": "missing_parenthesis"
     },
+
     "medium": {
         "id": "medium_001",
         "difficulty": "medium",
+        "grader": True,
+        "reward_range": (0.01, 0.99),
         "code": """
 arr = [1, 2, 3]
 for i in range(5):
@@ -15,9 +20,12 @@ for i in range(5):
 """,
         "issue": "index_out_of_range"
     },
+
     "hard": {
         "id": "hard_001",
         "difficulty": "hard",
+        "grader": True,
+        "reward_range": (0.01, 0.99),
         "code": """
 query = "SELECT * FROM users WHERE id=" + user_input
 """,
